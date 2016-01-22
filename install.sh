df569c2f44abba44c927306daa482ab173ff2280
Ariel Flesler <aflesler@gmail.com>
2016-01-22 20:39:32 -0300

    install.sh now supports but with and without -i

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 # Downloads all the dotfiles and copies them to ~
-# You can run it with this:
-# curl -s https://raw.githubusercontent.com/flesler/dotfiles/master/install.sh | sh
+#
+# Run it interactively (decide on conflicts):
+# curl -s https://raw.githubusercontent.com/flesler/dotfiles/master/install.sh | sh -i
+#
+# Remove the -u to automatically override any conflicted file
 
 DF=~/.dotfiles
 BKP=.bkp #~
-
-set -ieu
 
 if [ -d $DF ]; then
 	cd $DF
@@ -18,18 +19,20 @@ fi
 
 for src in $(find home ! -name home); do
 	dest=${src/home/~}
+
 	# Create directories if missing on destination
 	if [ -d "$src" ]; then
 		mkdir -p "$dest"
 		continue
 	fi
 
-	# If conflicted
-	if [ -f "$dest" ]; then
-
+	if [ ! -f "$dest" ]; then
+		:
+	elif cmp -s "$src" "$dest"; then
 		# Same file, auto-skip
-		cmp -s "$src" "$dest" && continue
-
+		continue
+	# If conflicted AND interactive
+	elif [ -n "$PS1" ]; then
 		echo -n "$dest is a file. (s)kip, (o)verwrite, (b)ackup?"
 		read -sn 1 -p ' ' chr
 		echo
@@ -38,15 +41,14 @@ for src in $(find home ! -name home); do
 			o) ;;
 			b) 
 				mv "$dest" "$dest$BKP"
-				echo "backed up $dest to $dest$BKP";;
+				echo "backed up to $dest$BKP";;
 			*)
 				echo "unknown letter '$chr', skipping"
 				continue;;
 		esac
-	else
-		echo "copying to $dest"
 	fi
 	
+	echo "copying to $dest"
 	cp "$src" "$dest"
 	# FIXME: Not really symlinking in Git Bash
 	#ln -sf "$src" "$dest"
